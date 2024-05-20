20e2fc309e16e60faabe252a4b10d77b47376a3a
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2024-05-19 22:08:04 -0300

    HeroUpdate

--- Element.java
+++
@@ -1,4 +0,0 @@
-public interface Element {
-    public void ElementSkillA(Enemy target);
-    public void ElementSkillB(Enemy target);
-}
--- Hero.java
+++ Hero.java
@@ -1,10 +1,48 @@
 public class Hero {
     private int hp;
     private String name;
+    private String gender;
+    private int baseAttack;
+    private int level;
+    private int exp;
+    private int maximumEXP;
 
-    public Hero(String name) {
-        this.hp = 30;
+    public Hero(int hp, String name, String gender) {
+        this.hp = hp;
         this.name = name;
+        this.gender = gender;
+        this.exp = 0;
+        this.level = 1;
+        this.baseAttack = 5;
+        this.maximumEXP = 10;
+    }
+
+    public int heroLevelUp() {
+        if (exp >= maximumEXP) {
+            exp = 0;
+            return level += 1;
+        } else
+            return 0;
+    }
+
+    public void upgradeStats() {
+        for (int i = 1; i <= level; i++) {
+            this.hp += 10;
+            this.baseAttack += 5;
+            this.maximumEXP += 50;
+        }
+    }
+
+    public String showStats() {
+        upgradeStats();
+        return "================\n" + "Hero Stats:\n" +
+                "Name: " + name + "\n" +
+                "Gender: " + gender + "\n" +
+                "Level: " + level + "\n" +
+                "HP: " + hp + "\n" +
+                "Base Attack: " + baseAttack + "\n" +
+                "EXP: " + exp + "/" + maximumEXP + "\n" +
+                "================\n";
     }
 
     public int getHp() {
@@ -22,4 +60,29 @@ public class Hero {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
 }
