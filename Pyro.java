f89a5f53231158cd5bb59f335329993916d86e84
Sakaef03 <103153340+Sakaef03@users.noreply.github.com>
2024-05-21 09:13:06 -0300

    Update Pyro.java

--- Pyro.java
+++ Pyro.java
@@ -1,21 +1,38 @@
+import java.util.Random;
+
 public class Pyro implements Element
 {
     private int burnDamage;
     private int buffValue;
     private int burnDuration;
     private boolean targetBurning;
+    private Random random;
+    private int accuracy;
 
     public Pyro()
     {
+        this.random = new Random();
         this.burnDamage = 2;
         this.burnDuration = 2;
         this.buffValue = 2;
         this.targetBurning = false;
+        this.accuracy = 30;
     }
 
     public int ElementSkillA()
     {
-        return this.burnDamage;
+        this.accuracy = random.nextInt(101);
+        if(this.accuracy >= 30)
+        {
+            this.targetBurning = true;
+            return this.burnDamage;
+        }
+
+        else
+        {
+            return 0;
+        }
+
     }
 
     public int ElementSkillB()
