7eac5751da6eca9e6827fad7dfe715bb4789e808
Sakaef03 <103153340+Sakaef03@users.noreply.github.com>
2024-05-21 09:14:27 -0300

    Added Battle and StatusChecker

---
+++ Battle.java
@@ -0,0 +1,12 @@
+public class Battle
+{
+    public static void main(String[] args)
+    {
+        Hero suzume = new Hero("Suzume", "Feminino");
+        suzume.setWeaponType(1);
+        Electro electro = new Electro();
+        Enemy roberto = new Enemy("Dendro", 20, 10, 0, 0, "Roberto", 5);
+        boolean firstSkipped;
+        boolean secondSkipped;
+    }
+}
